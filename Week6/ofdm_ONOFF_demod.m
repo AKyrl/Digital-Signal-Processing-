ba8cd958cda5773af9b5d6c0c7077a96b79b4210
pierre-edouard-walter <57531433+pierre-edouard-walter@users.noreply.github.com>
2019-12-06 10:10:51 +0100

    Add files via upload

---
+++ Week6/ofdm_ONOFF_demod.m
@@ -0,0 +1,95 @@
+function [qam_seq,channel_est]=ofdm_ONOFF_demod(mod_seq,N,L,Lqam,frame,Lt,Ld,freqR,start,stop,qamStream)
+ 
+mod_seq = reshape(mod_seq,N+L,[]);
+mod_seq = mod_seq(L+1:N+L,:); % L is gone 
+
+num=ceil(size(mod_seq,2)/(Lt+Ld));
+
+train1=zeros(N/2-1,1);
+for j=1:start
+train1(j)=frame(j);
+end
+for j=stop:N/2-1
+train1(j)=frame(j);
+end
+
+frame=train1;
+
+for i= 1: num-1 
+trainBlock =  mod_seq(:,1+(i-1)*(Lt+Ld):(i-1)*(Ld+Lt)+Lt); %Take each TrainBlock
+Packet = mod_seq(:,(i-1)*(Ld+Lt)+1+Lt:(i-1)*(Ld+Lt)+Ld+Lt); % Take each packet
+framec=conj(frame);
+framec=flip(framec,1);
+G = fft(trainBlock);
+B=G(1,:);
+h(1)=freqR(1);
+h(N/2)=freqR(N/2);
+
+    for k=1:N/2-1
+        A=repmat(frame(k),1,Lt);
+        B=G(k+1,:);
+        h(k+1)=B/A;
+        A2=repmat(framec(k),1,Lt);
+        B2=G(k+N/2+1,:);
+        h(k+N/2+1)=B2/A2;
+    end
+channel_est(i,:)=h;
+seq(:,(i-1)*Ld+1 :i*Ld) = fft(Packet)./h.'; %feeling seq mod_seq in seq
+end
+
+i=num;
+trainBlock =  mod_seq(:,1+(i-1)*(Lt+Ld):(i-1)*(Ld+Lt)+Lt); %Take the last TrainBlock
+CLeft= size(mod_seq,2) - (num-1)*(Ld+Lt)-Lt;
+Packet = mod_seq(:,(i-1)*(Ld+Lt)+1+Lt:(i-1)*(Ld+Lt)+Lt+CLeft); % Take the last packet
+framec=conj(frame);
+framec=flip(framec,1);
+G = fft(trainBlock);
+h(1)=freqR(1);
+h(N/2)=freqR(N/2);
+
+    for k=1:N/2-1
+        A=repmat(frame(k),1,Lt);
+        B=G(k+1,:);
+        h(k+1)=B/A;
+        A2=repmat(framec(k),1,Lt);
+        B2=G(k+N/2+1,:);
+        h(k+N/2+1)=B2/A2;
+    end
+channel_est(i,:)=h;
+seq(:,(i-1)*Ld+1 :(i-1)*Ld+CLeft) = fft(Packet)./h.'; %feeling mod_seq in seq
+
+seq1=seq;
+
+k=1;
+frames=size(seq1,2);
+
+for j=2:start
+for i=1:frames
+    qam_seq(k) = seq1(j,i);
+    k = k +1;
+end
+end
+
+for j=stop:N/2
+for i=1:frames
+
+   if k>Lqam
+       break;
+       
+   else
+        qam_seq(k) = seq1(j,i);
+     k=k+1;
+   end
+    
+end
+end
+
+qam_seq=qam_seq.';
+
+
+% sig = qam_seq(2:(N/2),:);
+% %sig = mod_seq(2:(N/2),1);
+% qam_seq = sig(:);
+% qam_seq=qam_seq(1:Lqam);
+
+ end
---
+++ Week6/ofdm_ONOFF_mod.m
@@ -0,0 +1,64 @@
+function [mod_seq,Dummy]=ofdm_mod(QAM_stream,N,L,train,Lt,Ld,start,stop)
+
+frames = ceil(length(QAM_stream)/((N/2-1 ) - (stop-start-1)));
+P = zeros(N/2-1, frames);
+k = 1;
+train1=zeros(N/2-1,1);
+for j=1:start
+for i=1:frames
+     P(j,i) = QAM_stream(k);
+     k=k+1;
+end
+train1(j)=train(j);
+end
+
+for j=stop:N/2-1
+for i=1:frames
+   if k>length(QAM_stream)
+       break;
+       
+   else
+     P(j,i) = QAM_stream(k);
+     k=k+1;
+   end 
+end
+train1(j)=train(j);
+end
+
+Pc= conj(P);
+Pc= flip(Pc,1);
+Packet=[zeros(1,size(P,2)); P; zeros(1,size(P,2)); Pc];
+
+
+Dummy=Packet; 
+
+
+trainB=repmat(train1,Lt,1);
+trainB=reshape(trainB,N/2-1,[]);
+trainBc=conj(trainB);
+trainBc=flip(trainBc,1);
+
+trainBlock=[zeros(1,size(trainB,2)); trainB; zeros(1,size(trainBc,2)); trainBc];
+
+num = ceil(size(Packet,2)/Ld); % number /Ld
+for i=1:num-1
+    Packet2(:,1+(i-1)*(Lt+Ld):(i-1)*(Ld+Lt)+Lt) =  trainBlock;
+    Packet2(:,(i-1)*(Ld+Lt)+1+Lt:(i-1)*(Ld+Lt)+Ld+Lt) = Packet(:,(i-1)*(Ld)+1:(i)*(Ld));
+end
+i=num;
+Packet2(:,1+(i-1)*(Lt+Ld):(i-1)*(Ld+Lt)+Lt) =  trainBlock;
+
+CLeft= size(Packet,2) - (num-1)*(Ld);
+
+Packet2(:,(i-1)*(Ld+Lt)+1+Lt:(i-1)*(Ld+Lt)+Lt+CLeft) = Packet(:,(i-1)*(Ld)+1:end);
+
+
+
+ifftP = ifft(Packet2); 
+ifftP=[ifftP(N-L+1:N,:);ifftP];
+
+mod_seq = ifftP(:);
+
+
+
+ end
